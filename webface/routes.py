cc702893dcab0cb52d1657b377117e3a764a78d0
Vencapython <vaclavlachman@seznam.cz>
2022-03-17 12:42:38 +0100

    Hotovo

--- webface/routes.py
+++ webface/routes.py
@@ -1,7 +1,10 @@
 from . import app
-from .models import User
+from .models import User, Addresses
+
 from flask import render_template, request, redirect, url_for, session, flash
 import functools
+import random
+import string
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -22,10 +25,61 @@ def prihlasit(function):
 @app.route("/", methods=["GET"])
 @db_session
 def index():
-    temp = []
-    for user in User.select():
-        temp.append([user.nick, user.passwd])
-    return render_template("base.html.j2", temp=temp)
+    shorcut = request.args.get('shorcut')
+    if shorcut and (url := Addresses.get(shorcut=shorcut).url):
+        print(url)
+    else:
+        url, shocut = None, None
+    if "nick" in session:
+        user = User.get(nick=session["nick"])
+        addresses = Addresses.select(lambda a: a.user == user)
+        return render_template("base.html.j2", url=url, shorcut=shorcut, addresses = list(addresses))
+    return render_template("base.html.j2", url=url, shorcut=shorcut)
+
+
+@app.route("/", methods=["POST"])
+@db_session
+def index_post():
+    url = request.form.get("url")
+    if url:
+        # vytvořím zkratku, která ještě neexistuje
+        shorcut = "".join([random.choice(string.ascii_letters) for i in range(7)])
+        address = Addresses.get(shorcut=shorcut)  # hledám jesli v DB náhodou takto zkratk až není
+        while address is not None:
+            shorcut = "".join([random.choice(string.ascii_letters) for i in range(7)])
+            address = Addresses.get(shorcut=shorcut)
+        
+        # Přidávám záznam do tabulky Addresses
+        if 'nick' in session:
+            address = Addresses(url=url, shorcut=shorcut, user=User.get(nick=session['nick']))
+        else:
+            address = Addresses(url=url, shorcut=shorcut)
+
+        return redirect(url_for("index", shorcut=shorcut))
+    else:
+        return redirect(url_for("index"))
+
+@app.route("/remove", methods = ["POST"])
+@db_session
+def remove_post():
+    if "nick" in session:
+        rmid = request.form.get("rmid")
+        user = User.get(nick=session["nick"])
+        addr = Addresses.get(id=rmid, user=user)
+        if addr:
+            addr.delete()
+    return redirect(url_for("index"))
+
+
+@app.route("/<path:shorcut>/", methods=["GET"])
+@db_session
+def shorcut_get(shorcut):
+    if url := Addresses.get(shorcut=shorcut).url:
+        return redirect(url)
+    else: 
+        return redirect(url_for("index"))
+
+
 
 
 @app.route("/add/", methods=["GET"])
@@ -43,7 +97,7 @@ def add_post():
     if not all([nick, passwd1, passwd2]):
         flash("Musíš vyplnit všechna políčka", "error")
     else:
-        user = User.get(nick=nick)
+        user = User.get(nick=nick)  # vrátí uživatele s nick == nick
         if user:
             flash("Tento uživatel již existuje", "error")
         elif passwd1 != passwd2:
